f5657f1a838dbf0c2fb92f5a03751a00fa68bea9
lejames <okejames55@gmail.com>
2022-12-15 16:07:33 +0300

    Remove unused variables

--- src/Map Components/AdminMap.js
+++ src/Map Components/AdminMap.js
@@ -5,8 +5,6 @@ import { xml2json } from 'xml-js';
 import Map from 'ol/Map'
 import View from 'ol/View'
 import TileLayer from 'ol/layer/Tile'
-import VectorLayer from 'ol/layer/Vector'
-import VectorSource from 'ol/source/Vector'
 import ImageLayer from 'ol/layer/Image';
 import ImageWMS from 'ol/source/ImageWMS';
 import OSM from 'ol/source/OSM';
